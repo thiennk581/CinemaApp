1a07e7b6117d6d341e9754f98b5f447481daa558
thiennk581 <thiennguen14@gmail.com>
2024-04-03 22:34:45 +0700

    test: test commit

--- app/src/main/java/com/example/momocinema/MainActivity.kt
+++ app/src/main/java/com/example/momocinema/MainActivity.kt
@@ -4,11 +4,24 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.outlined.Send
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
 import androidx.navigation.compose.rememberNavController
 import com.example.momocinema.data.Datasource
 import com.example.momocinema.navigation.CinemaTicketApp
@@ -37,7 +50,20 @@ class MainActivity : ComponentActivity() {
 val listFilm = Datasource().loadFilms()
 
 
+@Composable
+fun createComment(bodyComment: String, onValueChange:(String) -> Unit) {
+    OutlinedTextField(
+        value = bodyComment,
+        onValueChange = onValueChange,
+        placeholder = { Text(text = "Giờ là lúc ngôn từ lên ngôi ✍️")},
+        modifier = Modifier.fillMaxWidth(),
 
+        shape = RoundedCornerShape(10.dp),
+        trailingIcon = { IconButton(onClick = { /*TODO*/ }) {
+            Icon(imageVector = Icons.Outlined.Send, contentDescription = null)
+        }}
+    )
+}
 
 
 
@@ -47,6 +73,9 @@ val listFilm = Datasource().loadFilms()
 @Composable
 fun GreetingPreview() {
     MomoCinemaTheme {
+        var bodyComment by remember {
+            mutableStateOf("")
+        }
         //LogoCinema(Cinema("Cinestar", "Đồng Nai", "https://homepage.momocdn.net/blogscontents/momo-upload-api-210604170453-637584230934981809.png"))
         // selectDay()
         //listCinema(Datasource().loadCinemas())
@@ -59,5 +88,6 @@ fun GreetingPreview() {
         //filmComment(Datasource().loadComments()[0])
         //listCommentOfFilm(ranking = Ranking(averageRating = 9.3f, amount = 2200, star12 = 100, star34 = 100, star56 = 500, star78 = 600, star910 = 900), listComment = Datasource().loadComments())
         //ReviewsScreen(filmTitle = "Godzilla x Kong: Đế chế mới", ranking = Ranking(averageRating = 9.3f, amount = 2200, star12 = 100, star34 = 100, star56 = 500, star78 = 600, star910 = 900), listComment = Datasource().loadComments())
+        createComment(bodyComment = bodyComment, onValueChange = {bodyComment = it})
     }
 }
